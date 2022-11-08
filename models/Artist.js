4ec957203329960f93dcaa559879671bbcb6d7b5
rogseo <rogseo@gmail.com>
2022-11-08 09:52:15 -0600

    Artist model created

---
+++ models/Artist.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Artist extends Model {}
+
+Artist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    artist_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'artist',
+  }
+);
+
+module.exports = Artist;
